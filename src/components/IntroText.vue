8ca0721a2eb442c4e5b8dfaa4cf7ec48d7c7c1a7
Tor-Einar Skog <tor-einar.skog@nibio.no>
2022-06-28 14:53:32 +0200

    Added reference points. And the purpose feature

--- src/components/IntroText.vue
+++ src/components/IntroText.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="row">
         <div class="col">
+            <p>
             This is a tool for (slightly more) easily editing the DSS metadata. 
+            </p>
             <ol>
                 <li>
-                    (If you have no previous data entered, skip this step) To start editing, 
+                    <em>(If you have no previous data entered, skip this step)</em> To start editing, 
                     click the "Show YAML data" button. That opens up the text area where you can paste your current data.
                     Paste the data, and click "Load from YAML to form". The loading can take a few seconds, depending on
                     the complexity of the data.
@@ -16,6 +18,28 @@
                     Click the "Load from form to YAML" button. The data from the form is now ready to be copied and pasted
                     into your YAML file. If you don't have this already, create a file named [YOUR_DSS_NAME].yaml
                 </li>
+            </ol>
+            <p>
+            Fields that are visible to users in the platform are labeled with reference [#] to other documentation 
+            (<strong>TODO</strong>) with screen shots with corresponding labels.
+            </p>
+            <h4>Index of user visible parameters</h4>
+            <p>Parameters marked (*) appear to the user, but in a translated form (e.g. not EPPO codes, but species names)</p>
+            <ol>
+                <li>name</li>
+                <li>organization.name</li>
+                <li>organization.country</li>
+                <li>models[x].name</li>
+                <li>models[x].id</li>
+                <li>models[x].purpose</li>
+                <li>models[x].description</li>
+                <li>models[x].pests*</li>
+                <li>models[x].crops*</li>
+                <li>models[x].authors[x].name</li>
+                <li>models[x].authors[x].organization</li>
+                <li>models[x].valid_spatial.countries*</li>
+                <li>models[x].output.warning_status_interpretation</li>
+                
             </ol>
         </div>
     </div>
